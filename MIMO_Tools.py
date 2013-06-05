8b1848b07909c5c146d8c092b973f359936c7fc1
Samuel <u27105572@tuks.co.za>
2013-06-05 15:31:06 +0200

    testing

---
+++ MIMO_Tools.py
@@ -0,0 +1,165 @@
+'''
+Created on 27 Mar 2013
+
+@author: St Elmo Wilken
+'''
+
+
+import control as cn
+import numpy as np
+import scipy.linalg as spla
+import sympy as sm
+
+"""
+This toolbox assumes you have the control toolbox at your
+disposal... And all the imports above...
+"""
+
+
+def state_controllability(A, B):
+    """
+    Parameters: A => matrix A of state space representation
+                B => matrix B of state space representation 
+
+    Returns: state_control (Bool) => True if state controllable
+             in_pole_vec          => Input pole vectors for the states u_p_i
+             control_matrix       => State Controllability Matrix
+
+    This method checks if the state space description of the system
+    is state controllable according to Skogestad section 4.2.
+    
+    Note: This does not check for state controllability for systems with 
+    repeated poles
+
+    Note: The Gramian matrix type of solution has already been implemented by
+    the Control Toolbox folks.
+    """
+    state_control = True
+
+    A = np.asmatrix(A)
+    B = np.asmatrix(B)
+        
+    # Compute all input pole vectors.
+    ev, vl = spla.eig(A, left=True, right=False)
+    u_p = []
+    for i in range(vl.shape[1]):
+        vli = np.asmatrix(vl[:,i]) 
+        u_p.append(B.H*vli.T) 
+    state_control = not any(np.linalg.norm(x) == 0.0 for x in u_p)
+
+    # compute the controllability matrix
+    c_plus = [A**n*B for n in range(A.shape[1])]
+    control_matrix = np.hstack(c_plus)
+
+    return state_control, u_p, control_matrix
+
+
+def state_observability(A, C):
+    """
+    Parameters: A => state space matrix A
+                C => state space matrix C
+
+    Returns: state_obsr     => True is states are observable
+             out_pole_vec   => The output vector poles y_p_i
+             observe_matrix => The observability matrix
+    """
+    state_obsr = True
+
+    A = np.asmatrix(A)
+    C = np.asmatrix(C)
+
+    # compute all output pole vectors
+    ev, vr = spla.eig(A, left=False, right=True)
+    out_pole_vec = [np.around(C.dot(x), 3) for x in vr.T]
+    # TODO: is this calculation correct?
+    state_obsr = not any(np.sum(x)==0.0 for x in out_pole_vec)
+
+    # compute observability matrix
+    o_plus = [C*A**n for n in range(A.shape[1])]
+    observe_matrix = np.vstack(o_plus)
+
+    return state_obsr, out_pole_vec, observe_matrix
+
+
+def is_min_realisation(A, B, C):
+    """
+    Parameters: A => state space matrix
+                B =>        ''
+                C =>        ''
+
+    Returns: is_min_real => True if the system is the minimum realisation
+    """
+    state_obsr, out_pole_vec, observe_matrix = state_observability(A, C)
+    state_control, in_pole_vec, control_matrix = state_controllability(A, B)
+
+    return state_control and state_obsr
+
+
+def zero_directions(zero_vec, tf, e=0.0001):
+    """
+    Parameters: zero_vec => a vector containing all the transmission zeros of a system
+                tf       => the transfer function G(s) of the system
+                e        => this avoids possible divide by zero errors in G(z)
+    Returns:    zero_dir => zero directions in the form:
+                            (zero, input direction, output direction)
+    Notes: this method is going to give dubious answers if the function G has pole zero cancellation...
+    """
+    zero_dir = []
+    for z in zero_vec:
+        num, den = cn.tfdata(tf)
+        rows, cols = np.shape(num)
+
+        G = np.empty(shape=(rows, cols))
+
+        for x in range(rows):
+            for y in range(cols):
+                top = np.polyval(num[x][y], z)
+                bot = np.polyval(den[x][y], z)
+                if bot == 0.0:
+                    bot = e
+
+                entry = float(top) / bot
+                G[x][y] = entry
+
+        U, S, V = np.linalg.svd(G)
+        V = np.transpose(np.conjugate(V))
+        u_rows, u_cols = np.shape(U)
+        v_rows, v_cols = np.shape(V)
+        yz = np.hsplit(U, u_cols)[-1]
+        uz = np.hsplit(V, v_cols)[-1]
+        zero_dir.append((z, uz, yz))
+    return zero_dir
+
+def zero(A, B, C, D):
+    """
+    Computes the zeros of a transfer function in state space form.
+    Parameters: A, B, C, D state space matrices
+    Returns: zero vector (which you may use in my other functions)
+    """
+    z = sm.Symbol('z')
+    top = np.hstack((A,B))
+    bot = np.hstack((C,D))
+    m = np.vstack((top, bot))
+    M = sm.Matrix(m)
+    [rowsA, colsA] = np.shape(A)
+    [rowsB, colsB] = np.shape(B)
+    [rowsC, colsC] = np.shape(C)
+    [rowsD, colsD] = np.shape(D)
+    p1 = np.eye(rowsA)
+    p2 = np.zeros((rowsB, colsB))
+    p3 = np.zeros((rowsC, colsC))
+    p4 = np.zeros((rowsD, colsD))
+    top = np.hstack((p1,p2))
+    bot = np.hstack((p3,p4))
+    p = np.vstack((top, bot))
+    Ig = sm.Matrix(p)
+    zIg = z*Ig
+    f = zIg-M
+    zf = f.det()
+    zs = sm.solve(zf, z)
+    print "The zeros of the system are: "
+    for z in zs:
+        print z
+    return zs
+    
+    
--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ Created on Jan 27, 2012
 
 import numpy
 import scipy.signal
+import matplotlib.pyplot as plt
 
 
 #import control
@@ -14,48 +15,58 @@ import scipy.signal
 def circle(cx, cy, r):
     npoints = 100
     theta = numpy.linspace(0, 2*numpy.pi, npoints)
-    y = cx + numpy.sin(theta)*r
+    y = cy + numpy.sin(theta)*r
     x = cx + numpy.cos(theta)*r
     return x, y
 
-def distance_from_nominal(w, k, tau, theta, nom_response):
-    r = k/(tau*w*i + 1)*numpy.exp(-theta*w*i)
-    return numpy.abs(r - nom_response)
 
 def arrayfun(f, A):
-    """ recurses down to scalar elements in A, then applies f, returning lists containing the result"""
+    """
+    Recurses down to scalar elements in A, then applies f, returning lists
+    containing the result.
+    """
     if len(A.shape) == 0:
         return f(A)
     else:
         return [arrayfun(f, b) for b in A]
 
+
 def listify(A):
     return [A]
 
+
 def gaintf(K):
     r = tf(arrayfun(listify, K), arrayfun(listify, numpy.ones_like(K)))
+    return r
+
 
 def findst(G, K):
     """ Find S and T given a value for G and K """
     L = G*K
     I = numpy.eye(G.outputs, G.inputs)
-    S = inv(I + L)
+    S = numpy.linalg.inv(I + L)
     T = S*L
     return S, T
 
+
 def phase(G, deg=False):
-    return numpy.unwrap(numpy.angle(G, deg=deg), discont=180 if deg else numpy.pi)
+    return numpy.unwrap(numpy.angle(G, deg=deg), 
+                        discont=180 if deg else numpy.pi)
 
 
 def Closed_loop(Kz, Kp, Gz, Gp):
-    """Kz & Gz is the polynomial constants in the numerator
-    Kp & Gp is the polynomial constants in the denominator"""
+    """
+    Kz & Gz is the polynomial constants in the numerator
+    Kp & Gp is the polynomial constants in the denominator
+    """
 
-    # calculating the product of the two polynomials in the numerator and denominator of transfer function GK
+    # calculating the product of the two polynomials in the numerator
+    # and denominator of transfer function GK
     Z_GK = numpy.polymul(Kz, Gz)
     P_GK = numpy.polymul(Kp, Gp)
 
-    #calculating the polynomial of closed loop sensitivity function s = 1/(1+GK)
+    # calculating the polynomial of closed loop
+    # sensitivity function s = 1/(1+GK)
     Zeros_poly = Z_GK
     Poles_poly = numpy.polyadd(Z_GK, P_GK)
     return Zeros_poly, Poles_poly
@@ -80,14 +91,16 @@ def plot_freq_subplot(plt, w, direction, name, color, figure_num):
 
 
 def polygcd(a, b):
-    """ Find the Greatest Common Divisor of two polynomials using Euclid's algorithm:
+    """
+    Find the Greatest Common Divisor of two polynomials
+    using Euclid's algorithm:
     http://en.wikipedia.org/wiki/Polynomial_greatest_common_divisor#Euclidean_algorithm
-    
+
     >>> a = numpy.poly1d([1, 1]) * numpy.poly1d([1, 2])
     >>> b = numpy.poly1d([1, 1]) * numpy.poly1d([1, 3])
     >>> polygcd(a, b)
     poly1d([ 1.,  1.])
-    
+
     >>> polygcd(numpy.poly1d([1, 1]), numpy.poly1d([1]))
     poly1d([ 1.])
     """
@@ -101,79 +114,102 @@ def polygcd(a, b):
 
 
 class tf(object):
-    """ Very basic transfer function object 
-    
-    Construct with a numerator and denominator.  
+    """
+    Very basic transfer function object
+
+    Construct with a numerator and denominator:
 
     >>> G = tf(1, [1, 1])
     >>> G
     tf([ 1.], [ 1.  1.])
-    
+
     >>> G2 = tf(1, [2, 1])
-    
-    The object knows how to do addition:
+
+    The object knows how to do:
+
+    addition
     >>> G + G2
     tf([ 3.  2.], [ 2.  3.  1.])
     >>> G + G # check for simplification
     tf([ 2.], [ 1.  1.])
-    
+
     multiplication
     >>> G * G2
     tf([ 1.], [ 2.  3.  1.])
-    
+
     division
     >>> G / G2
     tf([ 2.  1.], [ 1.  1.])
-    
+
     Deadtime is supported:
     >>> G3 = tf(1, [1, 1], deadtime=2)
     >>> G3
     tf([ 1.], [ 1.  1.], deadtime=2)
-    
+
     Note we can't add transfer functions with different deadtime:
     >>> G2 + G3
     Traceback (most recent call last):
         ...
     ValueError: Transfer functions can only be added if their deadtimes are the same
-    
-    It is sometimes useful to define 
+
+    It is sometimes useful to define
     >>> s = tf([1, 0])
     >>> 1 + s
     tf([ 1.  1.], [ 1.])
-    
+
     >>> 1/(s + 1)
     tf([ 1.], [ 1.  1.])
     """
 
-    def __init__(self, numerator, denominator=1, deadtime=0):
-        """ Initialize the transfer function from a numerator and denominator polynomial """
+    def __init__(self, numerator, denominator=1, deadtime=0, name='', u='', y=''):
+        """
+        Initialize the transfer function from a
+        numerator and denominator polynomial
+        """
+        # TODO: poly1d should be replaced by numpy.polynomial.Polynomial
         self.numerator = numpy.poly1d(numerator)
         self.denominator = numpy.poly1d(denominator)
         self.simplify()
         self.deadtime = deadtime
+        self.name = name
+        self.u = u
+        self.y = y
 
     def inverse(self):
-        """ inverse of the transfer function """
+        """
+        Inverse of the transfer function
+        """
         return tf(self.denominator, self.numerator, -self.deadtime)
-    
+
     def step(self, *args):
+        """ Step response """ 
         return scipy.signal.lti(self.numerator, self.denominator).step(*args)
 
     def simplify(self):
         g = polygcd(self.numerator, self.denominator)
         self.numerator, remainder = self.numerator/g
         self.denominator, remainder = self.denominator/g
-
+    
     def __repr__(self):
-        r = "tf(" + str(self.numerator.coeffs) + ", " + str(self.denominator.coeffs)
+        if self.name:
+            r = str(self.name) + "\n"
+        else:
+            r = ''
+        r += "tf(" + str(self.numerator.coeffs) + ", " + str(self.denominator.coeffs)
         if self.deadtime != 0:
             r += ", deadtime=" + str(self.deadtime)
+        if self.u: 
+            r += ", u='" + self.u + "'"
+        if self.y: 
+            r += ", y=': " + self.y + "'"
         r += ")"
         return r
 
     def __call__(self, s):
-        """ This allows the transfer function to be evaluated at particular values of s
-        Effectively, this makes a tf object behave just like a function of s
+        """
+        This allows the transfer function to be evaluated at
+        particular values of s.
+        Effectively, this makes a tf object behave just like a function of s.
 
         >>> G = tf(1, [1, 1])
         >>> G(0)
@@ -189,7 +225,7 @@ class tf(object):
         if self.deadtime != other.deadtime:
             raise ValueError("Transfer functions can only be added if their deadtimes are the same")
         gcd = self.denominator * other.denominator
-        return tf(self.numerator*other.denominator + 
+        return tf(self.numerator*other.denominator +
                   other.numerator*self.denominator, gcd, self.deadtime)
 
     def __radd__(self, other):
@@ -197,7 +233,7 @@ class tf(object):
 
     def __sub__(self, other):
         return self + (-other)
-    
+
     def __rsub__(self, other):
         return other + (-self)
 
@@ -222,10 +258,101 @@ class tf(object):
     def __neg__(self):
         return tf(-self.numerator, self.denominator, self.deadtime)
 
+    def __pow__(self, other):
+        r = self
+        for k in range(other-1):
+            r = r * self
+        return r
 
-def feedback(forward, backward):
-    """ Calculate the feedback equivalent transfer function """
-    return forward/(1 + forward*backward)
+def feedback(forward, backward=None, positive=False):
+    """
+    Defined for use in connect function
+    Calculates a feedback loop
+    This version is for trasnfer function objects
+    Negative feedback is assumed, use positive=True for positive feedback
+    Forward refers to the function that goes out of the comparator
+    Backward refers to the function that goes into the comparator
+    """
+
+    # Create identity tf if no backward defined
+    if backward is None:
+        backward = 1
+    if positive:
+        backward = -backward
+    return  forward * 1/(1 + backward * forward)
+
+
+def tf_step(tf, t_final=10, initial_val=0, steps=100):
+    """
+    Prints the step response of a transfer function
+    """
+    # See the following docs for meaning of *args
+    # http://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.step.html
+    
+    # Surpress the complex casting error
+    import warnings
+    warnings.simplefilter("ignore")
+    # TODO: Make more specific
+    
+    tspace = numpy.linspace(0, t_final, steps)
+    foo = numpy.real(tf.step(initial_val, tspace))
+    plt.plot(foo[0], foo[1])
+    plt.show()
+
+# TODO: Concatenate tf objects into MIMO structure
+
+
+def sigmas(A):
+    """
+    Return the singular values of A
+
+    This is a convenience wrapper to enable easy calculation of
+    singular values over frequency
+
+    Example:
+    >> A = numpy.array([[1, 2],
+                        [3, 4]])
+    >> sigmas(A)
+    array([ 5.4649857 ,  0.36596619])
+
+    """
+    #TODO: This should probably be created with functools.partial
+    return numpy.linalg.svd(A, compute_uv=False)
+
+
+def feedback_mimo(forward, backward=None, positive=False):
+    """
+    Calculates a feedback loop
+    This version is for matrices
+    Negative feedback is assumed, use positive=True for positive feedback
+    Forward refers to the function that goes out of the comparator
+    Backward refers to the function that goes into the comparator
+    """
+
+    # Create identity matrix if no backward matrix is specified
+    if backward is None:
+        backward = numpy.asmatrix(numpy.eye(numpy.shape(forward)[0],
+                                  numpy.shape(forward)[1]))
+    # Check the dimensions of the input matrices
+    if backward.shape[1] != forward.shape[0]:
+        raise ValueError("The column dimension of backward matrix must equal row dimension of forward matrix")
+    forward = numpy.asmatrix(forward)
+    backward = numpy.asmatrix(backward)
+    I = numpy.asmatrix(numpy.eye(numpy.shape(backward)[0],
+                                 numpy.shape(forward)[1]))
+    if positive:
+        backward = -backward
+    return forward * (I + backward * forward).I
+
+
+def omega(w_start, w_end):
+    """
+    Convenience wrapper
+    Defines the frequency range for calculation of frequency response
+    Frequency in rad/time were time is the time unit used in model
+    """
+    omega = numpy.logspace(w_start, w_end, 1000)
+    return omega
 
 
 if __name__ == '__main__':
